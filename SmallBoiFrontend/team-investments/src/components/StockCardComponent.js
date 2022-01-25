a0b4163e9dbcc6dfb5aca4fd6d32425a0a5b94e1
Tiashka <seetaltiashka@gmail.com>
2022-01-25 11:36:03 +0200

    added the frontend component

---
+++ SmallBoiFrontend/team-investments/src/components/StockCardComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+import useStyles from "./StockCardComponentStyle";
+export default function StockCardComponent(props){
+    var classes = useStyles();
+    return (
+        <div className={classes.StockCardBorder}>
+test
+        </div>
+    )
+}
---
+++ SmallBoiFrontend/team-investments/src/components/StockCardComponentStyle.js
@@ -0,0 +1,11 @@
+import { makeStyles } from "@material-ui/styles";
+
+export default makeStyles(theme => ({
+
+  StockCardBorder:{
+    borderRadius: 25,
+    width: 36,
+    backgroundColor: "red",
+  }
+ 
+}));
